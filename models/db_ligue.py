d182f8ecfedc34e4ccda690049d4c19b82d86e29
quentin <SIO.gendreaud.quentin@gmail.com>
2015-10-16 15:57:51 +0200

    correction d'un bug sur la base de donnée

--- models/db_ligue.py
+++ models/db_ligue.py
@@ -12,19 +12,23 @@ db.define_table('ligue',
 db.define_table('facture',
                 Field('dateEdition','datetime'),
                 Field('dateEcheance', 'datetime'),
-                Field('dateReglement', 'datetime'))
+                Field('dateReglement', 'datetime')
+               ,migrate=False)
 
 db.define_table('relance',
                 Field('dateRelance','datetime'),
-                Field('numFacture','reference facture', requires=IS_IN_DB(db,db.facture.id)))
+                Field('numFacture','reference facture', requires=IS_IN_DB(db,db.facture.id))
+               ,migrate=False)
 
 db.define_table('affranchissement',
                 Field('libelle','string',requires=IS_NOT_EMPTY()),
-                Field('coutUnitaire','integer',requires=IS_NOT_EMPTY))
+                Field('coutUnitaire','integer',requires=IS_NOT_EMPTY)
+               ,migrate=False)
 
 db.define_table('reproduction',
                 Field('libelle','string',requires=IS_NOT_EMPTY()),
-                Field('PrixUnitaire','integer',requires=IS_NOT_EMPTY()))
+                Field('PrixUnitaire','integer',requires=IS_NOT_EMPTY())
+               ,migrate=False)
 
 db.define_table('prestation',
                 Field('quantite','integer'),
@@ -32,4 +36,5 @@ db.define_table('prestation',
                 Field('codeLigue','reference ligue',requires=IS_IN_DB(db,db.ligue.id)),
                 Field('codeFacture','reference facture',requires=IS_IN_DB(db,db.facture.id)),
                 Field('codeAffranchissement','reference affranchissement',requires=IS_IN_DB(db,db.affranchissement.id)),
-                Field('codeReproduction','reference reproduction',requires=IS_IN_DB(db,db.reproduction.id)))
+                Field('codeReproduction','reference reproduction',requires=IS_IN_DB(db,db.reproduction.id))
+               ,migrate=False)
