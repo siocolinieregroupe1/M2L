648d41100c85e772256197e830ae6b2a820187bf
quentin <SIO.gendreaud.quentin@gmail.com>
2015-10-16 15:41:33 +0200

    Ajout de table dans la bae de donnée

--- models/db_ligue.py
+++ models/db_ligue.py
@@ -6,5 +6,30 @@ db.define_table('ligue',
              Field('cp','string',requires=IS_NOT_EMPTY()),
              Field('tel','string',requires=IS_NOT_EMPTY()),
              Field('URLSiteWeb','string'),
-             Field('emailContact','string')  
+             Field('emailContact','string')
              ,migrate=False)
+
+db.define_table('facture',
+                Field('dateEdition','datetime'),
+                Field('dateEcheance', 'datetime'),
+                Field('dateReglement', 'datetime'))
+
+db.define_table('relance',
+                Field('dateRelance','datetime'),
+                Field('numFacture','reference facture', requires=IS_IN_DB(db,db.facture.id)))
+
+db.define_table('affranchissement',
+                Field('libelle','string',requires=IS_NOT_EMPTY()),
+                Field('coutUnitaire','integer',requires=IS_NOT_EMPTY))
+
+db.define_table('reproduction',
+                Field('libelle','string',requires=IS_NOT_EMPTY()),
+                Field('PrixUnitaire','integer',requires=IS_NOT_EMPTY()))
+
+db.define_table('prestation',
+                Field('quantite','integer'),
+                Field('datePrestation','datetime'),
+                Field('codeLigue','reference ligue',requires=IS_IN_DB(db,db.ligue.id)),
+                Field('codeFacture','reference facture',requires=IS_IN_DB(db,db.facture.id)),
+                Field('codeAffranchissement','reference affranchissement',requires=IS_IN_DB(db,db.affranchissement.id)),
+                Field('codeReproduction','reference reproduction',requires=IS_IN_DB(db,db.reproduction.id)))
